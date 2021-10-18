df54f072c5f1a5c36633bf125ac918b88846d9ba
Gayathri Balakumar <gayathri.balakumar@outlook.com>
2021-10-18 13:12:10 +0530

    assignment 2 ,3

---
+++ DotNetAppSqlDb/Migrations/202110130458127_AddProperty.cs
@@ -0,0 +1,24 @@
+﻿namespace DotNetAppSqlDb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProperty : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Todoes", "Name", c => c.String());
+            AddColumn("dbo.Todoes", "Brand", c => c.String());
+            AddColumn("dbo.Todoes", "ImagePath", c => c.String());
+            AddColumn("dbo.Todoes", "ThumbnailPath", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Todoes", "ThumbnailPath");
+            DropColumn("dbo.Todoes", "ImagePath");
+            DropColumn("dbo.Todoes", "Brand");
+            DropColumn("dbo.Todoes", "Name");
+        }
+    }
+}
---
+++ DotNetAppSqlDb/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get ; set; }
+        public byte[] ImagePath { get; set; }
+        
+        public string ThumbnailPath { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
